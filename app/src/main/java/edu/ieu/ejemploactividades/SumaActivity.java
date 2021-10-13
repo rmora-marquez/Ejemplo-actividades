c2b98b7341abd96db878bdd3fd24e23e6ab254ed
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-10-13 18:56:33 -0500

    interfaz SumaActivity

---
+++ app/src/main/java/edu/ieu/ejemploactividades/SumaActivity.java
@@ -0,0 +1,14 @@
+package edu.ieu.ejemploactividades;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SumaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_suma);
+    }
+}
