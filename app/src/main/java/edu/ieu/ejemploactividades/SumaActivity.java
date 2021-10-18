ffbca1fb6dce1148bb6031bc46e86b907a1b1ae9
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-10-18 18:54:15 -0500

    interfaz SumaActivity

--- app/src/main/java/edu/ieu/ejemploactividades/SumaActivity.java
+++ app/src/main/java/edu/ieu/ejemploactividades/SumaActivity.java
@@ -3,12 +3,57 @@ package edu.ieu.ejemploactividades;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SumaActivity extends AppCompatActivity {
+    private static final String TAG = SumaActivity.class.getSimpleName();
+    private static final String STATE_SUM_OUTPUT = "sumOutput";
+
+    private EditText mNumberAInput;
+    private EditText mNumberBInput;
+    private TextView mSumTotalLabel;
+    private Button mAddButton;
+    private Button mGetRootButton;
+
+    private String sumOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suma);
+        Log.d(TAG, "onCreate()");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+        Log.d(TAG, "onStart()");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        Log.d(TAG, "onResume()");
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        Log.d(TAG, "onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
     }
 }
